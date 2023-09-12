e7df602cc5b34725fcd17d76b5f109a5f8df685d
GuoxiangZ <106981818+GuoxiangZ@users.noreply.github.com>
2023-09-12 11:10:50 -0400

    creat Main.java, class main, and call doFizzBuzz, FizzBuzz.main() in this file.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        CallMain();
+    }
+    public static void CallMain(){
+        FizzBuzz.main(null);
+    }
+}
